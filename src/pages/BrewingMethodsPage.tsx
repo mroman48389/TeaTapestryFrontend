93aeb188ac2d6933397cffa1f38c3f50e93ca1d7
Mark Roman <mroman8465@gmail.com>
2026-04-13 13:00:58 -0400

    Accidentally removed formatting when experimenting on temporary message.

--- src/pages/BrewingMethodsPage.tsx
+++ src/pages/BrewingMethodsPage.tsx
@@ -6,7 +6,7 @@ export default function BrewingMethodsPage() {
     const underConstructionImg = 
         <div className="fade-in-component mt-5 flex flex-1 flex-col items-center">
             <h2 className="title--heading mb-3 text-center text-lg sm:text-xl md:text-2xl">
-                This area is currently under construction. Please visit "Tea profiles" in the meantime.
+                {"This area is currently under construction. Please visit \"Tea profiles\" in the meantime."}
             </h2>
 
             <img src={UnderConstructionImg} alt="Teacup with hardhat" className="h-auto w-100"/>
