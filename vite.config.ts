fb2c6d06b80385c976fb8677727350075b5f9936
Mark Roman <mroman8465@gmail.com>
2026-05-12 23:22:11 -0400

    Disabled react-docgen in production.

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,14 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
     plugins: [
-        react(), 
+        react({
+            /* Disable react-docgen in production. Prevent Netlify from running out of memory. */
+            babel: {
+                plugins: [],
+            },
+            jsxRuntime: "automatic",
+        }),
+
         tailwindcss(),
         sentryVitePlugin({
             org: "self-bst", // matches Sentry
