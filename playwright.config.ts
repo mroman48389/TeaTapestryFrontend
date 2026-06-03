1c261b0c785996494f7708c33175fde8f9fabe05
Mark Roman <mroman8465@gmail.com>
2026-06-03 16:34:05 -0400

    Added line to prevent overriding of CI env variables.

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,11 @@ import path from 'path';
 /* Load Playwright-specific .env file */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.resolve(__dirname, 'playwright_tests/.env') });
+/* Do not override CI env variables. */
+dotenv.config({ 
+    path: path.resolve(__dirname, 'playwright_tests/.env') ,
+    override: false,
+});
 
 export default defineConfig({
     testDir: './playwright_tests',
